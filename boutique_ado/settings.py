29fffada3dfeedcafbd3a9b739b96463497b68a3
José Luís Sousa <70488865+jltps@users.noreply.github.com>
2021-07-03 15:38:32 +0100

    Updated static files config

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
